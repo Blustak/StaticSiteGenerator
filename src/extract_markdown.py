53da6fca99add407ac0921e5571aeab02b3101ca
Blustak <menditta1@gmail.com>
2025-01-01 16:46:15 +0000

    finished markdown extraction funcs and tests

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -14,10 +14,18 @@ def extract_markdown_images(text):
     returns: [("cat", "www.cat.com/cat.jpg")]
     """
 
-    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
-    print(matches)
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     return matches
 
 
 def extract_markdown_links(text):
-    pass
+    """
+    returns a list of tuples, where each tuple contains the anchor text
+    and the URL.
+
+    Example: "This text contains [cat](www.cat.com/cat.jpg) a cool cat!"
+    returns: [("cat", "www.cat.com/cat.jpg")]
+    """
+
+    matches = re.findall(r"[^!]\[(.*?)\]\((.*?)\)", text)
+    return matches
--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from extract_markdown import extract_markdown_images
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 
 class TestExtractMarkdownImages(unittest.TestCase):
@@ -38,4 +38,63 @@ class TestExtractMarkdownImages(unittest.TestCase):
 
 
 class TestExtractMarkdownLinks(unittest.TestCase):
-    pass
+    def test_single_match(self):
+        text = """
+        a bit of text with a single link in it
+        this is [my link](www.google.com)
+        and this is the end of the string.
+        """
+
+        matches = extract_markdown_links(text)
+        expected = [("my link", "www.google.com")]
+        self.assertListEqual(matches, expected)
+
+    def test_boot_dev_example(self):
+        text = (
+            "This is text with a link [to boot dev]"
+            + "(https://www.boot.dev) and "
+            + "[to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        matches = extract_markdown_links(text)
+        expected = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev"),
+        ]
+        self.assertListEqual(matches, expected)
+
+    def test_multi_match(self):
+        text = """
+        This is a test with multiple [links](wikipedia.com)
+        there are many [links](wikipedia.com) in this, including
+        some mistyped ones like [this][google.com]. However,
+        my program should be able to identify 
+        [which ones are right](good.com), and which ones are
+        [bad(naughty.ru)
+        for example, this is a bad [link] (because there's a space!)
+        as is this one [here,
+        because it is spread over multiple lines]
+        """
+        matches = extract_markdown_links(text)
+        expected = [
+            ("links", "wikipedia.com"),
+            ("links", "wikipedia.com"),
+            ("which ones are right", "good.com"),
+        ]
+        self.assertListEqual(matches, expected)
+
+
+class TestExtractMarkdownBoth(unittest.TestCase):
+    def test_both(self):
+        text = """
+        this test checks to see if the functions can distinguish
+        between images and links in markdown.
+        an image is like this ![cat](cats.co.uk)
+        and a link is like this [dog](dogs.cz)
+        did you get it right?
+        """
+        links = extract_markdown_links(text)
+        images = extract_markdown_images(text)
+        expected_links = [("dog", "dogs.cz")]
+        expected_images = [("cat", "cats.co.uk")]
+        self.assertListEqual(links, expected_links)
+        self.assertListEqual(images, expected_images)
